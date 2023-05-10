6ec0a8adb4265fd9c20ec68dd74edb1482d7e17f
Steven Velozo <steven@velozo.com>
2023-05-10 05:29:50 -0700

    Adding harness data for more test cases

--- debug/Harness.js
+++ debug/Harness.js
@@ -1,3 +1,8 @@
-const libRetoldDataService = require('../source/Retold-Data-Service.js');
+const MaterialData = require(`./Materials.json`);
+const PayItemData = require(`./PayItems.json`);
 
-const _DataService = new libRetoldDataService(require('./Harness-Configuration.json'));
+for (let i = 0; i < PayItemData.length; i++)
+{
+    console.log(`Pay Item [${PayItemData[i].ItemCode}] [${PayItemData[i].Name}]`);
+}
+
