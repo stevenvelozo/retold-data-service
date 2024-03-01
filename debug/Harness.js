fa411e7f7a19781f3d137dcd298e58a4b91a1ee6
Steven Velozo <steven@velozo.com>
2024-03-01 15:37:57 -0800

    Upgraded orator

--- debug/Harness.js
+++ debug/Harness.js
@@ -3,33 +3,38 @@ const _Settings = require('./bookstore-configuration.json');
 const libFable = require('fable');
 
 _Fable = new libFable(_Settings);
+
 _Fable.serviceManager.addAndInstantiateServiceType('RetoldDataService', require('../source/Retold-Data-Service.js'));
 
-_Fable.MeadowEndpoints.Book.controller.BehaviorInjection.setBehavior('Read-PostOperation',
-    (pRequest, pRequestState, fComplete) =>
-    {
-        // Get the join records
-        _Fable.DAL.BookAuthorJoin.doReads(_Fable.DAL.BookAuthorJoin.query.addFilter('IDBook', pRequestState.Record.IDBook),
-            (pJoinReadError, pJoinReadQuery, pJoinRecords)=>
-            {
-                let tmpAuthorList = [];
-                for (let j = 0; j < pJoinRecords.length; j++)
-                {
-                    tmpAuthorList.push(pJoinRecords[j].IDAuthor);
-                }
-                if (tmpAuthorList.length < 1)
-                {
-                    pRequestState.Record.Authors = [];
-                    return fComplete();
-                }
-                else
-                {
-                    _Fable.DAL.Author.doReads(_Fable.DAL.Author.query.addFilter('IDAuthor', tmpAuthorList, 'IN'),
-                        (pReadsError, pReadsQuery, pAuthors)=>
-                        {
-                            pRequestState.Record.Authors = pAuthors;
-                            return fComplete();
-                        });
-                }
-            });
-    });
+_Fable.RetoldDataService.initializeService(
+	(pError) =>
+	{
+		_Fable.MeadowEndpoints.Book.controller.BehaviorInjection.setBehavior('Read-PostOperation',
+			(pRequest, pRequestState, fComplete) =>
+			{
+				// Get the join records
+				_Fable.DAL.BookAuthorJoin.doReads(_Fable.DAL.BookAuthorJoin.query.addFilter('IDBook', pRequestState.Record.IDBook),
+					(pJoinReadError, pJoinReadQuery, pJoinRecords)=>
+					{
+						let tmpAuthorList = [];
+						for (let j = 0; j < pJoinRecords.length; j++)
+						{
+							tmpAuthorList.push(pJoinRecords[j].IDAuthor);
+						}
+						if (tmpAuthorList.length < 1)
+						{
+							pRequestState.Record.Authors = [];
+							return fComplete();
+						}
+						else
+						{
+							_Fable.DAL.Author.doReads(_Fable.DAL.Author.query.addFilter('IDAuthor', tmpAuthorList, 'IN'),
+								(pReadsError, pReadsQuery, pAuthors)=>
+								{
+									pRequestState.Record.Authors = pAuthors;
+									return fComplete();
+								});
+						}
+					});
+			});
+	});
--- source/Retold-Data-Service.js
+++ source/Retold-Data-Service.js
@@ -8,6 +8,8 @@
 const libFableServiceProviderBase = require('fable-serviceproviderbase');
 
 const libOrator = require('orator');
+const libOratorServiceServerRestify = require('orator-serviceserver-restify');
+
 const libMeadow = require('meadow');
 const libMeadowEndpoints = require('meadow-endpoints');
 
@@ -34,97 +36,92 @@ class RetoldDataService extends libFableServiceProviderBase
 
 		this.options = this.fable.Utility.extend(defaultDataServiceSettings, this.options);
 
-		// TODO: Check if Fable already has an Orator service provider, otherwise add one.
-		// TODO: First switch the new Orator to the new service provider pattern
-		// TODO: Make the restify/http stuff optional, so IPC works.
-		// Orator is the (usually web) server
-		this.fable.Orator = new libOrator(this.fable, require('orator-serviceserver-restify'));
+		// Add the restify server provider and orator base class to fable
+		this.fable.serviceManager.addServiceType('OratorServiceServer', libOratorServiceServerRestify);
+		this.fable.serviceManager.addServiceType('Orator', libOrator);
 
-		// Because this requires the callback, we will have to add better plumbing for these services to initialie
-		this.fable.Orator.initializeServiceServer(() =>
-		{
-			// TODO: This code will be much cleaner with these as services
-			this._Meadow = libMeadow.new(_Fable);
+		// Initialize Restify
+		this.fable.serviceManager.instantiateServiceProvider('OratorServiceServer', this.options);
 
-			// Create DAL objects for each table in the schema
-			// These will be unnecessary when meadow and meadow-endpoints are full fledged fable services
-			this._DAL = {};
-			this.fable.DAL = this._DAL;
-			this._MeadowEndpoints = {};
-			this.fable.MeadowEndpoints = this._MeadowEndpoints;
+		// Initialize Orator, which will automatically use the default `OratorServiceServer` service we just instantiated
+		this.fable.serviceManager.instantiateServiceProvider('Orator', this.options);
 
-			// TODO: Change this to an option (e.g. we might want to do ALASQL)
-			// Load the mysql connection for meadow if it doesn't exist yet
-			_Fable.serviceManager.addAndInstantiateServiceType('MeadowMySQLProvider', require('meadow-connection-mysql'));
+		// TODO: This code will be much cleaner with meadow and meadow-endpoints as services
+		this._Meadow = libMeadow.new(_Fable);
 
-			this.fullModel = false;
-			this.entityList = false;
-
-			this.serviceInitialized = false;
-
-			if (this.options.AutoInitializeDataService)
-			{
-				this.fable.log.info(`The Retold Data Service is Auto Initializing itself`);
-				this.initializeService();
-			}
-
-			if (this.options.AutoStartOrator)
-			{
-				this.fable.log.info(`The Retold Data Service is Auto Starting Orator`);
-				this.fable.Orator.startWebServer(
-					(pError) =>
-					{
-						if (pError)
-						{
-							console.log(`Error auto-starting Orator: ${pError}`, pError);
-						}
-					});
-			}
-		});
+		// Create DAL objects for each table in the schema
+		// These will be unnecessary when meadow and meadow-endpoints are full fledged fable services
+		this._DAL = {};
+		this.fable.DAL = this._DAL;
+		this._MeadowEndpoints = {};
+		this.fable.MeadowEndpoints = this._MeadowEndpoints;
 	}
 
-	initializeService()
+	initializeService(fCallback)
 	{
 		if (this.serviceInitialized)
 		{
 			this.fable.log.error("Retold Data Service Application is being initialized but has already been initialized...");
+			return fCallback(new Error("Retold Data Service Application is being initialized but has already been initialized..."));
 		}
 		else
 		{
-			this.fable.log.info("Retold Data Service Application is starting up...");
+			this.fable.log.info(`The Retold Data Service is Auto Starting Orator`);
+			this.fable.Orator.startWebServer(
+				(pError) =>
+				{
+					if (pError)
+					{
+						console.log(`Error auto-starting Orator: ${pError}`, pError);
+						return fCallback(pError);
+					}
 
-			// Create DAL objects for each table in the schema
+					// TODO: Change this to an option (e.g. we might want to do ALASQL)
+					// Load the mysql connection for meadow if it doesn't exist yet
+					_Fable.serviceManager.addAndInstantiateServiceType('MeadowMySQLProvider', require('meadow-connection-mysql'));
 
-			// 1. Load full compiled schema of the model from stricture
-			_Fable.log.info(`...loading full model stricture schema...`);
-			this.fullModel = require (`${this.options.FullMeadowSchemaPath}${this.options.FullMeadowSchemaFilename}`);
-			_Fable.log.info(`...full model stricture schema loaded.`);
+					this.fullModel = false;
+					this.entityList = false;
 
-			// 2. Extract an array of each table in the schema
-			_Fable.log.info(`...getting entity list...`);
-			this.entityList = Object.keys(this.fullModel.Tables);
+					this.serviceInitialized = false;
 
-			// 3. Enumerate each entry in the compiled model and load a DAL for that table
-			_Fable.log.info(`...initializing ${this.entityList.length} DAL objects and corresponding Meadow Endpoints...`);
-			for (let i = 0; i < this.entityList.length; i++)
-			{
-				// 4. Create the DAL for each entry (e.g. it would be at _DAL.Movie for the Movie entity)
-				let tmpDALEntityName = this.entityList[i];
-				let tmpDALPackageFile = `${this.options.DALMeadowSchemaPath}${this.options.DALMeadowSchemaPrefix}${tmpDALEntityName}${this.options.DALMeadowSchemaPostfix}.json`
-				_Fable.log.info(`Initializing the ${tmpDALEntityName} DAL from [${tmpDALPackageFile}]...`);
-				this._DAL[tmpDALEntityName] = this._Meadow.loadFromPackage(tmpDALPackageFile);
-				// 5. Tell this DAL object to use MySQL
-				_Fable.log.info(`...defaulting the ${tmpDALEntityName} DAL to use MySQL`);
-				this._DAL[tmpDALEntityName].setProvider('MySQL');
-				// 6. Create a Meadow Endpoints class for this DAL
-				_Fable.log.info(`...initializing the ${tmpDALEntityName} Meadow Endpoints to use MySQL`);
-				this._MeadowEndpoints[tmpDALEntityName] = libMeadowEndpoints.new(this._DAL[tmpDALEntityName]);
-				// 8. Expose the meadow endpoints on Orator
-				_Fable.log.info(`...mapping the ${tmpDALEntityName} Meadow Endpoints to Orator`);
-				this._MeadowEndpoints[tmpDALEntityName].connectRoutes(this.fable.Orator.webServer);
-			}
+					this.fable.log.info("Retold Data Service Application is starting up...");
 
-			this.serviceInitialized = true;
+					// Create DAL objects for each table in the schema
+
+					// 1. Load full compiled schema of the model from stricture
+					_Fable.log.info(`...loading full model stricture schema...`);
+					this.fullModel = require (`${this.options.FullMeadowSchemaPath}${this.options.FullMeadowSchemaFilename}`);
+					_Fable.log.info(`...full model stricture schema loaded.`);
+
+					// 2. Extract an array of each table in the schema
+					_Fable.log.info(`...getting entity list...`);
+					this.entityList = Object.keys(this.fullModel.Tables);
+
+					// 3. Enumerate each entry in the compiled model and load a DAL for that table
+					_Fable.log.info(`...initializing ${this.entityList.length} DAL objects and corresponding Meadow Endpoints...`);
+					for (let i = 0; i < this.entityList.length; i++)
+					{
+						// 4. Create the DAL for each entry (e.g. it would be at _DAL.Movie for the Movie entity)
+						let tmpDALEntityName = this.entityList[i];
+						let tmpDALPackageFile = `${this.options.DALMeadowSchemaPath}${this.options.DALMeadowSchemaPrefix}${tmpDALEntityName}${this.options.DALMeadowSchemaPostfix}.json`
+						_Fable.log.info(`Initializing the ${tmpDALEntityName} DAL from [${tmpDALPackageFile}]...`);
+						this._DAL[tmpDALEntityName] = this._Meadow.loadFromPackage(tmpDALPackageFile);
+						// 5. Tell this DAL object to use MySQL
+						_Fable.log.info(`...defaulting the ${tmpDALEntityName} DAL to use MySQL`);
+						this._DAL[tmpDALEntityName].setProvider('MySQL');
+						// 6. Create a Meadow Endpoints class for this DAL
+						_Fable.log.info(`...initializing the ${tmpDALEntityName} Meadow Endpoints to use MySQL`);
+						this._MeadowEndpoints[tmpDALEntityName] = libMeadowEndpoints.new(this._DAL[tmpDALEntityName]);
+						// 8. Expose the meadow endpoints on Orator
+						_Fable.log.info(`...mapping the ${tmpDALEntityName} Meadow Endpoints to Orator`);
+						this._MeadowEndpoints[tmpDALEntityName].connectRoutes(this.fable.Orator.webServer);
+					}
+
+					this.serviceInitialized = true;
+
+					return fCallback(pError);
+				});
 		}
 	}
 }
